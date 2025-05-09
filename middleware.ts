73baf791ea010cb6ab59e41127ab7a728576db9f
FireTime <143462801+firetimeofficial@users.noreply.github.com>
2025-05-09 13:27:21 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs"
 import type { Database } from "@/lib/types"
-import { supabaseUrl, supabaseAnonKey } from "@/lib/env"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
